e69e971d4f0bb87e155a324f6f40ef00ee6f7947
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 13:39:16 -0400

    Update CORE Pools address

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -13,21 +13,7 @@ const pools: PoolConfig[] = [
     poolCategory: PoolCategory.CORE,
     harvest: true,
     sortOrder: 1,
-    tokenPerBlock: '2.3148',
-  },
-  {
-    sousId: 0,
-    stakingToken: tokens.cake,
-    earningToken: tokens.cake,
-    contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '10',
-    sortOrder: 1,
-    isFinished: false,
+    tokenPerBlock: '1',
   },
 ]
 
