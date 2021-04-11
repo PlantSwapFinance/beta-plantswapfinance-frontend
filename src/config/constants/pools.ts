7bb9be0635f8379d6adfdd9c77094f201d5a7548
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 13:58:08 -0400

    update plant garden test address

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -8,7 +8,7 @@ const pools: PoolConfig[] = [
     earningToken: tokens.plant,
     contractAddress: {
       56: '0x58BA5Bd8872ec18BD360a9592149daed2fC57c69',
-      97: '0xFe5Ab583d91fa90549aC61666CF1C4e2CeA5187e',
+      97: '0xd6756c4876ACD3c0162AF74Ba25b6b78F951836b',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
