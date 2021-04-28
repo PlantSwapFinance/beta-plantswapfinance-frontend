7d042ee96b6a89f4ae5155c94c33382c4cc74b65
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-28 18:32:10 -0400

    Adding BNB and BUSD to the pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -13,7 +13,33 @@ const pools: PoolConfig[] = [
     poolCategory: PoolCategory.CORE,
     harvest: true,
     sortOrder: 1,
-    tokenPerBlock: '0.01',
+    tokenPerBlock: '0.004',
+  },
+  {
+    sousId: 0,
+    stakingToken: tokens.wbnb,
+    earningToken: tokens.plant,
+    contractAddress: {
+      56: '0x58BA5Bd8872ec18BD360a9592149daed2fC57c69',
+      97: '0xd6756c4876ACD3c0162AF74Ba25b6b78F951836b',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 1,
+    tokenPerBlock: '0.002285',
+  },
+  {
+    sousId: 0,
+    stakingToken: tokens.busd,
+    earningToken: tokens.plant,
+    contractAddress: {
+      56: '0x58BA5Bd8872ec18BD360a9592149daed2fC57c69',
+      97: '0xd6756c4876ACD3c0162AF74Ba25b6b78F951836b',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    sortOrder: 1,
+    tokenPerBlock: '0.002285',
   },
 ]
 
