50b30d7cb17ac7b7956c18df2f5c9cc98226dcfc
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-29 10:33:56 -0400

    Add latest news

--- src/views/Home/components/NewsCard.tsx
+++ src/views/Home/components/NewsCard.tsx
@@ -29,6 +29,8 @@ const NewsCard = () => {
           {TranslateString(542, 'News & Announcement')}
         </Heading>
         <Block>
+          <Label><a href="https://plantswapfinance.medium.com/opening-of-the-garden-new-plant-look-and-more-b8b9ff554e16">Opening of the Garden, new PLANT look, and more!</a></Label>
+          <br />
           <Label><a href="https://plantswapfinance.medium.com/are-you-farming-plant-7e6f2dd8a7f5">Are you farming 🚜 $PLANT 🌱</a></Label>
           <br />
           <Label><a href="https://app.bounce.finance/fixed-swap/5279">🌱PLANT Token available on Bounce.finance!</a></Label>
@@ -36,7 +38,7 @@ const NewsCard = () => {
           <Label><a href="https://plantswapfinance.medium.com/plant-token-as-been-deploy-on-binance-smart-chain-d81a3d202d96">🌱PLANT Token as been deploy on Binance Smart Chain</a></Label>
           <br />
           <Label><a href="https://plantswapfinance.medium.com/plantswap-finance-is-sprouting-7b3bee286d4d">🌱 PlantSwap.finance is sprouting 🌱</a></Label>
-        </Block>
+          </Block>
       </CardBody>
     </StyledNewsCard>
   )
