84ca8ac0799efa3c9e932ffad2709551e78536f7
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-07-17 12:26:23 -0400

    Add some vertical garden on top of homepage

--- src/views/VerticalGarden/VerticalGarden.tsx
+++ src/views/VerticalGarden/VerticalGarden.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useRef  } from 'react'
+import React, { useState, useMemo, useEffect, useRef } from 'react'
 import { Helmet } from "react-helmet";
 import { Route, useRouteMatch } from 'react-router-dom'
 import BigNumber from 'bignumber.js'
