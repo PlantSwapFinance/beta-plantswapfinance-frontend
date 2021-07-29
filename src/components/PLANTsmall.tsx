1464f75b02a42ec66ce5880bd9ebede0d3b7152b
PlantSwapFinance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-07-29 18:06:12 -0400

    Add a bit more 3d

--- src/components/PLANTsmall.tsx
+++ src/components/PLANTsmall.tsx
@@ -1,8 +1,39 @@
 import * as THREE from 'three'
 import React, { useRef } from 'react'
-import { Canvas, useFrame  } from '@react-three/fiber';
+import { Canvas, extend, useThree, useFrame, ReactThreeFiber  } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// https://spectrum.chat/react-three-fiber/general/property-orbitcontrols-does-not-exist-on-type-jsx-intrinsicelements~44712e68-4601-4486-b4b4-5e112f3dc09e
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<
+        OrbitControls,
+        typeof OrbitControls
+      >;
+    }
+  }
+}
+
+interface OrbitRef {
+  obj: {
+    update(): void
+  }
+}
+
+extend({ OrbitControls })
+
+const Controls: React.FC<any> = (props) => {
+  const ref = useRef<OrbitRef>(null)
+  const { camera, gl } = useThree()
+  useFrame(() => {
+    ref.current?.obj?.update()
+  })
+  return <orbitControls ref={ref} args={[camera, gl.domElement]} {...props} />
+}
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -19,6 +50,7 @@ function Model(props: JSX.IntrinsicElements['group']) {
   const mesh = useRef<THREE.Mesh>()
   useFrame(() => {
     mesh.current.rotation.z += 0.025
+    mesh.current.rotation.x += 0.01
   })
   return (
     <group ref={group} {...props} dispose={null}>
@@ -32,12 +64,13 @@ const Plant: React.FC = () => (
     <ambientLight intensity={0.25} />
     <pointLight castShadow intensity={0.7} position={[10, 10, 10]} />
     <Model />
+    <Controls position={[0, 0, 2]} />
   </Canvas>
 )
 
 const CanvaStyle = {
-  "width": "100%",
-  "height": "128px"
+  "width": "72px",
+  "height": "72px"
 }
 
 export default Plant;
